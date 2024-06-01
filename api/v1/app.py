1bf861a9f67f0e2b9e63e8ff54d498da6a4fa4fe
alpine57 <kamutileeon@gmail.com>
2024-06-01 01:48:43 -0700

    corrections

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""to start your API!"""
+"""
+This module sets up a Flask application and configures routing.
+"""
+
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
 import os
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
 
 @app.teardown_appcontext
 def teardown_db(exception):
@@ -19,4 +20,3 @@ if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
-

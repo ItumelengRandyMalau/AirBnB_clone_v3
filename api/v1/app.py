a32f1e1d2916c5a6fd0b35aba32d4d351b2ff511
alpine57 <kamutileeon@gmail.com>
2024-06-01 01:44:24 -0700

    corrections

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-
+"""to start your API!"""
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
 import os
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown_db(exception):

88ee024f8835631b52a5c62a353918e450f6f2ee
alpine57 <kamutileeon@gmail.com>
2024-06-01 02:04:54 -0700

    shell

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,13 +8,21 @@ from models import storage
 from api.v1.views import app_views
 import os
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
     """Closes the storage on teardown"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handles 404 errors and returns a JSON-formatted response"""
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')

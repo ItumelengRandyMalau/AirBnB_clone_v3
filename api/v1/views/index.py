e59b018ab561b39bdf0285906711b01583d3a7fb
alpine57 <kamutileeon@gmail.com>
2024-06-01 01:51:34 -0700

    lt

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+"""This module sets up a Flask application and configures routing"""
 from api.v1.views import app_views
 from flask import jsonify
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():

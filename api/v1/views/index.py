849239f98f67736d979aebb39e46f45f07b67757
alpine57 <kamutileeon@gmail.com>
2024-06-01 01:55:59 -0700

    lt

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
 
-"""This module sets up a Flask application and configures routing"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
+    """Returns the status of the API"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """Retrieves the number of each objects by type"""
+    counts = {
+        "Amenity": storage.count("Amenity"),
+        "City": storage.count("City"),
+        "Place": storage.count("Place"),
+        "Review": storage.count("Review"),
+        "State": storage.count("State"),
+        "User": storage.count("User")
+    }
+    return jsonify(counts)
+
